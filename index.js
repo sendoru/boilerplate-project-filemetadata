880a9fc91559dd2434653de133139d258cf7aa62
sendoru <sendol39@gmail.com>
2024-02-26 11:19:48 +0000

    Update

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const https = require('https');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+require('dotenv').config();
+
+const options = {
+  key: fs.readFileSync('./rootca.key'),
+  cert: fs.readFileSync('./rootca.crt')
+};
 
 var app = express();
+
+const upload = multer({ dest: 'uploads/' })
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +26,12 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  })
+})
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+https.createServer(options, app).listen(3000);
